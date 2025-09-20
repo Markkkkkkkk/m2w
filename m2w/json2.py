7a82c3b5a3ff671757a59f66ed837300732cf133
jiaohongyan <1033987763@qq.com>
2025-09-20 11:35:46 +0800

    1，legacy_web01.json保存内容ASCII字符改成UTF-8正常显示，方便管理。

--- m2w/json2.py
+++ m2w/json2.py
@@ -16,7 +16,7 @@ def read_json_as_dict(path_file):
 
 def save_dict_as_json(json_dict, path_file):
     """
-    将dict保存为json文件
+    将dict保存为json文件，UTF-8 并保持中文可读
     """
-    with open(path_file, "w") as json_file:
-        json.dump(json_dict, json_file, indent=4)
+    with open(path_file, "w", encoding="utf-8") as json_file:
+        json.dump(json_dict, json_file, indent=4, ensure_ascii=False)
